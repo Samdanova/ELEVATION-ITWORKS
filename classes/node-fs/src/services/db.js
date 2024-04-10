3f681ece36247dff7fe52510f6b06894887b0d06
Samdanova <samdanova123@gmail.com>
2024-04-10 22:04:09 +0300

    edit

--- classes/node-fs/src/services/db.js
+++ classes/node-fs/src/services/db.js
@@ -23,4 +23,22 @@ async function deleteData(path) {
   getDb().delete(path);
 }
 
+async function saveToArray(pathToArr, item, idKey) {
+  let array = db.getData(pathToArr) || [];
+  const id = item[idKey];
+  if (!id) {
+    throw new Error(`Item does not have ${idKey}`);
+  }
+  const existingIndex = array.findIndex((i) => i[idKey] === id);
+  if (existingIndex !== -1) {
+    throw new Error(`Item with ${idKey} ${id} already exists`);
+  }
+  array.push(item);
+  db.push(pathToArr, array);
+}
+async function removeFromArray(pathToArr, id, idKey) {}
+async function getArray(pathToArr, limit, page) {} //liimit 500
+async function updateArray(pathToArr, id, item, idKey) {}
+async function getOneFromArray(pathToArr, id) {}
+
 export { storeData, getData, deleteData };
